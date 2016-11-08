dca2127502eee5d4eae9ff394b62bfbef1744195
Simon Rasmussen <simon@huebanden.dk>
2016-11-08 12:40:19 +0100

    version 1

---
+++ BioTrio/src/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Simon_
+ */
+public class NewClass {
+    public NewClass(){
+        System.out.println("ey");
+    }
+}
