0f759a50f1722962b75c08627d6e9685f81eaada
Simon Rasmussen <simon@huebanden.dk>
2016-11-18 12:58:10 +0100

    GUI Log in page finished

---
+++ src/Model/Hall.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Simon_
+ */
+public class Hall {
+    private String hallName;
+    private int hallNumber;
+    private int takenSeats;
+    private int totalSeats;
+
+    public String getHallName() {
+        return hallName;
+    }
+
+    public void setHallName(String hallName) {
+        this.hallName = hallName;
+    }
+
+    public int getHallNumber() {
+        return hallNumber;
+    }
+
+    public void setHallNumber(int hallNumber) {
+        this.hallNumber = hallNumber;
+    }
+
+    public int getTakenSeats() {
+        return takenSeats;
+    }
+
+    public void setTakenSeats(int takenSeats) {
+        this.takenSeats = takenSeats;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+}
--- src/View/NewJFrame.java
+++ src/View/NewJFrame.java
@@ -47,6 +47,18 @@ public class NewJFrame extends javax.swing.JFrame {
         psswrdLbl = new javax.swing.JLabel();
         psswrdTxtPnl = new javax.swing.JPanel();
         passwordField = new javax.swing.JPasswordField();
+        hostLbl = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        hostTxtPnl = new javax.swing.JPanel();
+        hostTF = new javax.swing.JTextField();
+        portPnl = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        portTF = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        dbNameTF = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(244, 51, 55));
@@ -116,6 +128,49 @@ public class NewJFrame extends javax.swing.JFrame {
 
         pnlC.add(psswrdTxtPnl);
 
+        hostLbl.setPreferredSize(new java.awt.Dimension(80, 26));
+
+        jLabel1.setText("Hostname");
+        hostLbl.add(jLabel1);
+
+        pnlC.add(hostLbl);
+
+        hostTF.setPreferredSize(new java.awt.Dimension(155, 26));
+        hostTxtPnl.add(hostTF);
+
+        pnlC.add(hostTxtPnl);
+
+        portPnl.setPreferredSize(new java.awt.Dimension(80, 26));
+
+        jLabel2.setText("Port");
+        portPnl.add(jLabel2);
+
+        pnlC.add(portPnl);
+
+        portTF.setPreferredSize(new java.awt.Dimension(155, 26));
+        jPanel1.add(portTF);
+
+        pnlC.add(jPanel1);
+
+        jPanel2.setPreferredSize(new java.awt.Dimension(80, 26));
+
+        jLabel3.setText("Database Name");
+        jPanel2.add(jLabel3);
+
+        pnlC.add(jPanel2);
+
+        jPanel3.setPreferredSize(new java.awt.Dimension(155, 26));
+
+        dbNameTF.setPreferredSize(new java.awt.Dimension(155, 26));
+        dbNameTF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dbNameTFActionPerformed(evt);
+            }
+        });
+        jPanel3.add(dbNameTF);
+
+        pnlC.add(jPanel3);
+
         getContentPane().add(pnlC, java.awt.BorderLayout.CENTER);
 
         pack();
@@ -129,7 +184,10 @@ public class NewJFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
         String user = usrTxt.getText();
         String password = new String(passwordField.getText());
-        if(DB_Connection.connect(user, password) != null){
+        String hostName = hostTF.getText();
+        String port = portTF.getText();
+        String DBname = dbNameTF.getText();
+        if(DB_Connection.connect(user, password, hostName, port, DBname) != null){
             JOptionPane.showMessageDialog(null,"User found");
             setVisible(false);
         }
@@ -141,6 +199,10 @@ public class NewJFrame extends javax.swing.JFrame {
     private void passwordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordFieldActionPerformed
+
+    private void dbNameTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dbNameTFActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_dbNameTFActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,12 +242,24 @@ public class NewJFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel btnPnl;
     private javax.swing.JButton cnclBtn;
+    private javax.swing.JTextField dbNameTF;
+    private javax.swing.JPanel hostLbl;
+    private javax.swing.JTextField hostTF;
+    private javax.swing.JPanel hostTxtPnl;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JButton okBtn;
     private javax.swing.JPasswordField passwordField;
     private javax.swing.JPanel pnlC;
     private javax.swing.JPanel pnlE;
     private javax.swing.JPanel pnlN;
     private javax.swing.JPanel pnlW;
+    private javax.swing.JPanel portPnl;
+    private javax.swing.JTextField portTF;
     private javax.swing.JPanel psswrLblPnl;
     private javax.swing.JLabel psswrdLbl;
     private javax.swing.JPanel psswrdTxtPnl;
